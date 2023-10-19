fc30437c4909674ba7cf4574690f86e426f5ffbc
mathis marichy <marichy.pro@gmail.com>
2023-10-19 11:53:48 +0200

    Mise en fonction des appels API | categorie modal via appel API

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,64 +1,62 @@
 // mettre dans une fonction
-fetch('http://localhost:5678/api/works').then((res) => {
-    res.json().then((data) => {
+function updateWork() {
+    fetch('http://localhost:5678/api/works').then((res) => {
+        res.json().then((data) => {
 
-        const gallery = document.querySelector('.gallery')
-        // console.log(data)
-        
-        data.forEach((work) => {
+            const gallery = document.querySelector('.gallery')            
+            data.forEach((work) => {
+                    const figure = document?.createElement('figure');
+                    figure.dataset.id = work.id;
+                    figure.dataset.cat = work.categoryId;
+                    figure.classList.add('work');
+                    const imgWork = document?.createElement('img');
+                    imgWork.src = work.imageUrl;
 
-                const figure = document?.createElement('figure');
-                figure.dataset.id = work.id;
-                figure.dataset.cat = work.categoryId;
-                figure.classList.add('work');
-                const imgWork = document?.createElement('img');
-                imgWork.src=work.imageUrl;
+                    const titleLeg = document.createElement('figcaption');
+                    titleLeg.textContent = work.title;
 
-                const titleLeg = document.createElement('figcaption');
-                titleLeg.textContent = work.title;
+                    figure.appendChild(imgWork);
+                    figure.appendChild(titleLeg);
+                    gallery.appendChild(figure);
 
-                
-
-                figure.appendChild(imgWork);
-                gallery.appendChild(figure);
-                figure.appendChild(titleLeg);
-                // console.log(work);
-        });
+            });
+        })
     })
-})
+}
+function updateCategories() {
+    fetch ('http://localhost:5678/api/categories').then((res) =>{
+        res.json().then((data) => {
 
-fetch ('http://localhost:5678/api/categories').then((res) =>{
-    res.json().then((data) => {
+            const filtre = document.querySelector('.filters');
 
-        const filtre = document.querySelector('.filters');
-        // console.log(data)
-        data.forEach((categorie) => {
-            let catFiltre = document.createElement('button');
-            catFiltre.dataset.id = categorie.id;
-            catFiltre.classList.add('filter__btn');
-            catFiltre.addEventListener("click", function(){
-                document.querySelectorAll(".filter__btn").forEach(btn => {
-                    btn.classList.remove("filter__btn--active");})
-                catFiltre.classList.add('filter__btn--active');
-                const works = document.querySelectorAll('.work');
-                works.forEach(work => {
-                    work.style.display = 'block';
-                    if (work.dataset.cat != categorie.id){
-                        work.style.display = 'none';
-                    }
+            data.forEach((categorie) => {
+                let catFiltre = document.createElement('button');
+                catFiltre.dataset.id = categorie.id;
+                catFiltre.classList.add('filter__btn');
+                catFiltre.addEventListener("click", function(){
+
+                    document.querySelectorAll(".filter__btn").forEach(btn => {
+                        btn.classList.remove("filter__btn--active");
+                    })
+                    catFiltre.classList.add('filter__btn--active');
+                    const works = document.querySelectorAll('.work');
+                    works.forEach(work => {
+                        work.style.display = 'block';
+                        if (work.dataset.cat != categorie.id){
+                            work.style.display = 'none';
+                        }
+                    })
+                    
                 })
-                
-            })
 
-            let tButton = document.createTextNode(categorie.name);
+                let tButton = document.createTextNode(categorie.name);
 
-            catFiltre.appendChild(tButton);
-            filtre.appendChild(catFiltre);
-            // console.log(categorie);
-        });
+                catFiltre.appendChild(tButton);
+                filtre.appendChild(catFiltre);
+            });
+        })
     })
-})
-
+}
 let allWork = document.querySelector('.allWork');
 allWork.addEventListener('click', function(){
     document.querySelectorAll(".filter__btn").forEach(btn => {
@@ -90,6 +88,7 @@ function adminH (){
             portfolioEdition.style.display = "flex"
         }
     }
+
 // Ajout d'un EventListener pour se déconnecter et rester sur la page //
 LogOut.addEventListener('click', function() {
     if (localStorage.getItem("token")) {
@@ -103,10 +102,8 @@ LogOut.addEventListener('click', function() {
 
 
 // modale //
-let modal1 = null
-let modal2 = null
-
-
+let modal1 = null;
+let modal2 = null;
 
 function openModal(modal){
     event.preventDefault();
@@ -114,75 +111,85 @@ function openModal(modal){
     modal.style.display = null;
     modal.removeAttribute('aria-hidden');
     modal.setAttribute('aria-modal', 'true');
-    modal.addEventListener('click', closeModal2);
-    modal.querySelector('.js-modal-close').addEventListener('click', closeModal2);
+
+    modal.querySelector('.js-modal-close').addEventListener('click', () => closeModal(modal));
     modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
-    console.log(modal)
+    modal.addEventListener('click', () => closeModal(modal));
+
+    if (modal.id === "modal2"){
+        modal1 = document.getElementById('modal1')
+        modal1.setAttribute('aria-hidden', true);
+        modal1.removeAttribute('aria-modal');
+        modal1.removeEventListener('click', () => closeModal(modal1));
+        modal1.style.display = "none";
+    }
 }
+
 function closeModal(modal){
+
     modal.setAttribute('aria-hidden', true);
     modal.removeAttribute('aria-modal');
     modal.removeEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', () => closeModal(modal));
     modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+
     const hideModal = function(){
+        if (modal.id === "modal2"){
+            let modal2ToUnDisplay = document.querySelector('#modal1')
+            modal2ToUnDisplay.style.display = "none";
+            modal2ToUnDisplay.removeEventListener('animationend', hideModal);
+        }
         modal.style.display = "none";
         modal.removeEventListener('animationend', hideModal);
         modal = null;
     }
     modal.addEventListener('animationend', hideModal)
-
 }
 
+document.querySelector('.js-modal').addEventListener('click', () => openModal(modal1))
+document.querySelector('.js-button-ajouter').addEventListener('click', () => openModal(modal2));
 
-const openModal1 = function (e) {
-    e.preventDefault();
-    openModal(modal1);
-};
-const closeModal1 = function (e) {
-    if (modal1 === null) return;
-    e.preventDefault();
-    closeModal(modal1);
-}
-
-const openModal2 = function (e) {
-    openModal(modal2)
-
-    // modal1 = document.getElementById('modal1')
-    // modal1.setAttribute('aria-hidden', true);
-    // modal1.removeAttribute('aria-modal');
-    // modal1.removeEventListener('click', closeModal1);
-    // modal1.style.display = "none";
-}
-const closeModal2 = function(e) {
-    if (modal2 === null) return;
-    e.preventDefault();
-    closeModal(modal2)
-    console.log(modal2)
-}
-
-
-
-
-document.querySelector('.js-button-ajouter').addEventListener('click', openModal2);
 
 const stopPropagation = function (e){
     e.stopPropagation();
 }
 
-document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal1);
-});
-
 window.addEventListener('keydown', function (e){
     if (e.key === "Escape" || e.key === "Esc") {
-    closeModal1(e);
+        modal1 = document.getElementById('modal1')
+        modal1.style.display = "none"
+        modal2 = document.getElementById('modal2')
+        modal2.style.display = "none"
     }
 })
 
-window.addEventListener('keydown', function (e){
-    if (e.key === "Escape" || e.key === "Esc") {
-    closeModal2(e);
-    }
+let back = document.querySelector('.js-modal-back').addEventListener('click', function (){
+    modal2 = document.getElementById('modal2')
+    modal1.removeAttribute('aria-hidden')
+    modal1.style.display = "flex"
+    modal2.style.display = "none"
 })
 
+
+// Categories ajout work
+
+let categorieModal = document.getElementById('categorie')
+fetch ('http://localhost:5678/api/categories').then((res) =>{
+        res.json().then((data) => {
+            const categorieModal = document.getElementById('categorie')
+            console.log(data)
+
+            data.forEach((cat) => {
+                let catModal = document.createElement('option');
+                catModal.dataset.id = cat.id;
+                let catName = document.createTextNode(cat.name);
+
+                catModal.appendChild(catName)
+                categorieModal.appendChild(catModal)
+            })
+        })
+    })
+
+
+updateWork()
+updateCategories()
